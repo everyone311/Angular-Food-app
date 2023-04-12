b8e9571c99d39d9019036ee945741cb01a6a1c11
Aarthi Manivannan <aarthi.m@zucisystems.com>
2023-04-12 15:24:11 +0530

    Register page resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
-
 import { FooterComponent } from './footer/footer.component';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     RegisterPageComponent,
-
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-
     HomepageComponent,
     HeaderComponent,
-
+    FooterComponent,
   ],
+
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, IonicModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,22 +1,69 @@
-import { AbstractControl,ValidatorFn } from "@angular/forms"
-export class Validation{
-static match(controlName:string,checkControlName:string):ValidatorFn{
-return(controls:AbstractControl)=>{
-const control=controls.get(controlName);
-const checkControl=controls.get(checkControlName);
-console.log('validation Called')
-if(checkControl?.errors && !checkControl.errors['matching'])
-{
-return null;
-}
-if(control?.value !==checkControl?.value){
-console.log('if block')
-controls.get(checkControlName)?.setErrors({matching:true});
-return {matching:true};
-}
-else
-return null;
-}
-}
+import { Component } from '@angular/core';
 
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+  AbstractControl,
+} from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+
+  templateUrl: './register-page.component.html',
+
+  styleUrls: ['./register-page.component.css'],
+})
+export class RegisterPageComponent {
+  form: FormGroup = new FormGroup({
+    Uname: new FormControl(),
+
+    Pno: new FormControl(),
+
+    Email: new FormControl(),
+
+    pWord: new FormControl(),
+
+    cpWord: new FormControl(),
+  });
+
+  constructor(private Lf: FormBuilder, public router: Router) {}
+
+  ngOnInit(): void {
+    this.form = this.Lf.group(
+      {
+        Uname: ['', [Validators.required]],
+
+        Email: ['', [Validators.required, Validators.email]],
+
+        Pno: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(10),
+          ],
+        ],
+
+        pWord: ['', [Validators.required]],
+
+        cpWord: ['', [Validators.required]],
+      } // { //   validator: Validation.match("password", "confirmPassword") // }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  submit: boolean = false;
+
+  onSubmit(): void {
+    this.submit = true;
+
+    this.form.reset();
+  }
 }
