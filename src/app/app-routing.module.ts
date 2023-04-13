543be6b39d40aa47f5d0d94b29580fccd8f304f3
Denil Doss <denil.doss@zucisystems.com>
2023-04-13 21:29:47 +0530

    view-details page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ViewFoodsComponent } from './view-foods/view-foods.component';
 
 const routes: Routes = [
   { path: 'Login', component: LoginPageComponent },
   { path: 'Register', component: RegisterPageComponent },
   { path: 'Home', component: HomepageComponent },
-
+  {path:'viewFoods',component:ViewFoodsComponent},
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
 ];
 
